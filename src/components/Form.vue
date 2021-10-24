fe273e5ba394dda64711415798b8df6191a989dd
giggity-hub <imigstefan@gmail.com>
2021-10-24 14:07:56 +0200

    init

---
+++ src/components/Form.vue
@@ -0,0 +1,59 @@
+<script >
+import {ref} from 'vue';
+import 'axios'
+// const descrconstiption = ref("")
+// const title = ref("")
+
+
+// function submit(){
+//     console.log(title,description);
+// }
+
+export default {
+  data() {
+    return {
+      description: "",
+      title: "",
+    };
+  },
+  methods: {
+    submit() {
+    console.log(this.title); // logs the input value
+    },
+  },
+};
+</script>
+
+<template>
+
+    <div id="form" class="bg-red-500 w-full max-w-3xl p-4">
+        <h3 class="label">Title</h3>
+        <input v-model="title" class="text-input" type="text" name="" id="">
+
+        <h3 class="label">Description</h3>
+        <textarea v-model="description" class="text-input" name="" id="" cols="30" rows="10"></textarea>
+
+        <button @click="submit" class="btn btn-blue my-3 ">Submit</button>
+    
+    </div>
+</template>
+
+<style scoped>
+
+.text-input{
+    @apply bg-gray-400 rounded w-full 
+}
+.label{
+    @apply text-white py-3
+}
+
+.btn {
+    @apply font-bold py-2 px-4 rounded;
+}
+.btn-blue {
+@apply bg-blue-500 text-white;
+}
+.btn-blue:hover {
+@apply bg-blue-700;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import Form from './Form.vue';
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <!-- <h1>{{ msg }}</h1> -->
+
+  <div class="bg-orange">
+    <Form/>
+  </div>
+
+  <!-- reddit -->
+  <!-- twitter -->
+  <!-- discord -->
+  <!-- email -->
+
+
+</template>
+
+<style scoped>
+
+</style>
